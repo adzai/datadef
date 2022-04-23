eeacb3c4b081f7557f1f924cc199349f89b4b46c
Adam Zaiter <zaiter.adam7@gmail.com>
2022-04-23 20:39:58 +0200

    add mutable

--- examples/server.rkt
+++ examples/server.rkt
@@ -15,7 +15,7 @@
 (define (get-users req)
   (define dat1 (datadef:users->result #:json #t))
   (displayln (format "DATA 1: ~v" dat1))
-  (define dat2 (datadef:users->result #:json #t))
+  (define dat2 (datadef:users->result #:json #t 3 #:mutable #t))
   (displayln (format "DATA 2: ~v" dat2))
   (response/jsexpr (make-immutable-hash `([data . ,dat1]))))
 
@@ -65,7 +65,7 @@
     (check-false (exported? 'datadef:single-user)))
   (test-case
     "Testing users servlet"
-    (parameterize ([db-mocking-data (make-immutable-hash `((,datadef:users . ((0 1) 1))
+    (parameterize ([db-mocking-data (make-immutable-hash `((datadef:users . ((0 1) 1))
                                           #;(dtb-query-rows . ,(make-list 2 (list (vector 1 "adam" 7))))))])
       (define req (make-request #"GET" (string->url "http://racket-lang.org")
                                 '() (delay #t) #f "" 1111 ""))
